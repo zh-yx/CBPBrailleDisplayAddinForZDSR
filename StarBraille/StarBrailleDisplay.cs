00edbc394f0cc08c977b07db8c90b2f45bd6b780
Zhang Yunxi <zhyx-work@outlook.com>
2026-05-15 19:31:12 +0800

    Add StarBrailleDisplay class.

---
+++ StarBraille/StarBrailleDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StarBraille
+{
+    internal class StarBrailleDisplay
+    {
+        private const int ROW_COUNT = 1;
+        private const int CELL_COUNT = 40;
+
+        private readonly Action<int, int> _setRowCellHandle;
+        private readonly Action<string> _actionHandler;
+        private readonly Action<int> _routingKeyHandler;
+
+        public StarBrailleDisplay(Action<int, int> setHandle, Action<string> actionHandler, Action<int> routingKeyHandler)
+        {
+            _setRowCellHandle = setHandle;
+            _actionHandler = actionHandler;
+            _routingKeyHandler = routingKeyHandler;
+        }
+
+        public bool Connect()
+        {
+            if (StarBrailleDriver.OpenBrailleDisplay() != 1)
+            {
+                return false;
+            }
+
+            _setRowCellHandle(ROW_COUNT, CELL_COUNT);
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            StarBrailleDriver.CloseBrailleDisplay();
+        }
+
+        public void ShowBraille(byte[] cells)
+        {
+            StarBrailleDriver.ShowBraille(cells);
+        }
+    }
+}
